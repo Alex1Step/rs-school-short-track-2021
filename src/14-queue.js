71970cd66c117272c5508bcb9e77f442441237c3
Alex_Stepanchuk <666stepashka666@gmail.com>
2021-04-12 10:04:43 +0300

    fix: 14 is done

--- src/14-queue.js
+++ src/14-queue.js
@@ -1,4 +1,4 @@
-// const ListNode = require('../extensions/list-node');
+const ListNode = require('../extensions/list-node');
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
  *
@@ -12,17 +12,52 @@
  */
 
 class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
   get size() {
     throw new Error('Not implemented');
   }
 
-  enqueue(/* element */) {
-    throw new Error('Not implemented');
+  enqueue(elem) {
+    const newNode = new ListNode(elem);
+
+    if (!this.head || !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+
+    this.tail.next = newNode;
+
+    this.tail = newNode;
   }
 
   dequeue() {
-    throw new Error('Not implemented');
+    if (!this.head) {
+      return this;
+    }
+
+    const deletedHead = this.head;
+
+    if (this.head.next) {
+      this.head = this.head.next;
+    } else {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return deletedHead.value;
   }
 }
 
 module.exports = Queue;
+// const queue = new Queue();
+// queue.enqueue(5);
+// queue.enqueue(6);
+// queue.enqueue(7);
+// // eslint-disable-next-line no-console
+// console.log(queue.dequeue());
+// // eslint-disable-next-line no-console
+// console.log(queue.dequeue());
